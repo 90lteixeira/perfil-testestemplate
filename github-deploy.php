89c27996b75150f0dcc369ef6ace17149c8a8217
Leonardo Teixeira <leoh.web@gmail.com>
2017-10-19 20:31:52 -0200

    melhorias

--- github-deploy.php
+++ github-deploy.php
@@ -1,5 +1,5 @@
 <?php
 
 $path = '/home/admin/web/lteixeira.tk/public_html/';
-$output = shell_exec('cd ' . $path . ' && sudo git pull origin master 2>&1');
+$output = shell_exec('cd ' . $path . ' && git pull origin master 2>&1');
 echo "<pre>$output</pre>";
