864499111abcb0559c15851ee1e644523d6f885b
Leonardo Teixeira <leoh.web@gmail.com>
2017-10-19 20:15:40 -0200

    confg git auto pull

--- github-deploy.php
+++ github-deploy.php
@@ -1,4 +1,4 @@
 <?php
 
-$path = '/html/admin/web/lteixeira.tk/public_html/';
+$path = '/home/admin/web/lteixeira.tk/public_html/';
 shell_exec('cd ' . $path . ' && sudo git reset --hard HEAD && sudo git pull origin master 2>&1');
